23566bb8f22c2bd5a7371a53d8935b7bb382c3c3
unknown <gh2525@nyu.edu>
2026-02-10 20:07:47 -0600

    py

--- webServer.py
+++ webServer.py
@@ -6,72 +6,53 @@ import sys
 
 
 def webServer(port=13331):
-  serverSocket = socket(AF_INET, SOCK_STREAM)
-  
-  #Prepare a server socket
-  serverSocket.bind(("", port))
-  
-  #Fill in start
-  serverSocket.listen(1)
-  #Fill in end
+    serverSocket = socket(AF_INET, SOCK_STREAM)
 
-  while True:
-    #Establish the connection
-    
-    print('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept()
-    
-    try:
-      message = #Fill in start -a client is sending you a message   #Fill in end 
-      filename = message.split()[1]
-      
-      #opens the client requested file. 
-      #Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:],     #fill in start              #fill in end   )
-      
-      
+    # Prepare a server socket
+    serverSocket.bind(("", port))
 
-      #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
-      #Fill in start 
-              
-      #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+    # Fill in start
+    serverSocket.listen(1)
+    # Fill in end
 
+    while True:
+        # Establish the connection
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()   # accepting connections
 
-      #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
- 
-      #Fill in end
-               
-      for i in f: #for line in file
-      #Fill in start - append your html file contents #Fill in end 
-        
-      #Send the content of the requested file to the client (don't forget the headers you created)!
-      #Send everything as one send command, do not send one line/item at a time!
+        try:
+            message = connectionSocket.recv(1024).decode()   # receive client message
+            filename = message.split()[1]
 
-      # Fill in start
+            # Open the requested file (read as bytes)
+            f = open(filename[1:], "rb")
 
+            # HTTP response header for a valid request
+            header = b"HTTP/1.1 200 OK\r\n"
+            header += b"Content-Type: text/html; charset=UTF-8\r\n"
+            header += b"\r\n"   # blank line ends the header
 
-      # Fill in end
-        
-      connectionSocket.close() #closing the connection socket
-      
-    except Exception as e:
-      # Send response message for invalid request due to the file not being found (404)
-      # Remember the format you used in the try: block!
-      #Fill in start
+            # Read file contents
+            filedata = f.read()
+            f.close()
 
-      #Fill in end
+            # Send headers + file content in ONE send call
+            connectionSocket.sendall(header + filedata)
 
+            connectionSocket.close()
 
-      #Close client socket
-      #Fill in start
+        except Exception as e:
+            # Send 404 Not Found response
+            header = b"HTTP/1.1 404 Not Found\r\n"
+            header += b"Content-Type: text/html; charset=UTF-8\r\n"
+            header += b"\r\n"
 
-      #Fill in end
+            body = b"<html><body><h1>404 Not Found</h1></body></html>"
 
-  # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop. 
-  # DO NOT PLACE ANYWHERE ELSE AND DO NOT UNCOMMENT WHEN SUBMITTING, YOU ARE GONNA HAVE A BAD TIME
-  #serverSocket.close()
-  #sys.exit()  # Terminate the program after sending the corresponding data
+            connectionSocket.sendall(header + body)
+
+            # Close client socket
+            connectionSocket.close()
 
 if __name__ == "__main__":
-  webServer(13331)
+    webServer(13331)
